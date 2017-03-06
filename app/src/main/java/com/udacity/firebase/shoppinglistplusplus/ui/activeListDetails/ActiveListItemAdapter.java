72f2a690b60f6d7ca97b31950e5ebe7b74e44457
chrishad95 <chrishadley@gmail.com>
2017-03-06 15:22:02 -0500

    working on list items

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeListDetails/ActiveListItemAdapter.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeListDetails/ActiveListItemAdapter.java
@@ -2,9 +2,11 @@ package com.udacity.firebase.shoppinglistplusplus.ui.activeListDetails;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.Query;
+import com.udacity.firebase.shoppinglistplusplus.R;
 import com.udacity.firebase.shoppinglistplusplus.model.ShoppingListItem;
 
 /**
@@ -28,9 +30,11 @@ public class ActiveListItemAdapter extends FirebaseListAdapter<ShoppingListItem>
     @Override
     protected void populateView(View view, final ShoppingListItem item, int position) {
         // TODO Set up the view so that it shows the name of the item and the trash can button.
+
+        TextView ItemNameTextView = (TextView) view.findViewById(R.id.text_view);
+
         // The trashcan button should trigger a dialog to appear.
         // The code to make the dialog appear is commented out below.
-
 //        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mActivity, R.style.CustomTheme_Dialog)
 //                .setTitle(mActivity.getString(R.string.remove_item_option))
 //                .setMessage(mActivity.getString(R.string.dialog_message_are_you_sure_remove_item))
